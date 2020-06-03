e69ddc05c3cffe426b2e3334c9aee2924fec926b
Michael <mhmichaelhuynh@gmail.com>
2020-06-03 15:16:53 -0400

    Clean up code

--- client/App.js
+++ client/App.js
@@ -1,48 +1,15 @@
 import React, { Component } from 'react';
 import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
-import InstagramLogin from 'react-native-instagram-login'
-import store from 'react-native-simple-store'
 import axios from 'axios'
 
 
 export default class App extends Component {
-  /*const client = axios.create({
-    baseURL: 'http://localhost:3000',
-    json: true
-  })
-
-  data = () => {
-    return {
-      response: 'No data yet...'
-    }
-  }
-
-  const sendRequest = () => {
-    client({
-      method: 'get',
-      url: '/'
-    }).then((res) => {
-      this.response = res.data.message
-    }).catch((error) => {
-      this.response = error
-    })
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button 
-      onPress= {sendRequest}
-      title= "Send Request"
-      />
-    </View>
-  );*/
-  
   constructor(props) {
     super(props)
     this.state = {
-      token: ''
+      token: '',
+      show: false
     }
   }
 
@@ -51,45 +18,28 @@ export default class App extends Component {
     await store.save('igUserId', data.user_id)
     this.setState({ igToken: data.access_token, igUserId: data.user_id})
   }
+  
   render () {
+    onSignInButtonClick = () => {
+      console.log('Magic!!!')
+    }
+
   return (
-    // <WebView source={{
-    //   uri: 'https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code'
-    // }} 
-    // style={{ marginTop: 40 }}/>
-  //   <View style={styles.container}>
-     
-  //   style={{ marginTop: 20 }}/> */}
-  //   {/* <Text>Open up App.js to start working on your app!</Text>
-  //     <Button 
-  //     onPress= {onSignInButtonClick}
-  //     title= "Send Request"
-  //     />
-  //   </View>
-  <View>
-  <TouchableOpacity onPress={()=> this.instagramLogin.show()}>
-      <Text style={{color: 'red'}}>Login</Text>
-      <Text style={{color: 'red'}}>this.state.token</Text>
-  </TouchableOpacity>
-  <InstagramLogin
-      ref={ref => (this.instagramLogin = ref)}
-      appId='550046499039084'
-      appSecret='bffcbe0912d7d4e3e53c2be5825f678a'
-      redirectUrl='https://www.google.com'
-      scopes={['user_profile', 'user_media']}
-      onLoginSuccess={ this.setIgToken }
-      onLoginFailure={(data) => console.log(data)}
-  />
-</View>
-   )
-}
-}
+    <View style={styles.container}>
+      <WebView source={{ uri: 'https://www.google.com/' }} />
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <Button style={{marginTop:'50px'}} onPress= {onSignInButtonClick} title= "Send Request"/> */}
+    </View>
+  );
+}}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop : 50
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
